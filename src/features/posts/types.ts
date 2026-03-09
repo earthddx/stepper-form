1b444a51b8d85bb3d5d251489343a5a68611aac6
Artem Murzo <murzotom@rocketmail.com>
2026-03-09 16:52:53 -0400

    cleanup

--- src/features/posts/types.ts
+++
@@ -1,7 +0,0 @@
-import { z } from "zod";
-
-import type { postSchema } from "./schema";
-
-export type PostFormData = z.infer<typeof postSchema>;
-
-export type Post = PostFormData & { id: number; createdAt: string };
